b414a93f49d216ab240762737c92d38ecad00381
zaqmjuop <1060007045@qq.com>
2018-06-09 09:45:40 +0800

    添加编译后的文件

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import toPicker from './element';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const input2 = document.querySelector('#input2');
-  const input3 = document.querySelector('#input3');
-  toPicker(input2);
-  toPicker(input3);
+  const input = document.querySelector('#input');
+  toPicker(input);
 });
